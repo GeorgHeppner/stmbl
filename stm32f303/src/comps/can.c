529506b265e82997cd83ef9bdb096d12c636fa5b
Georg Heppner <heppner@fzi.de>
2017-10-24 12:23:15 +0200

    Refactoring reset functionality and mode set functionality REMOVED TYPO!!!!

--- stm32f303/src/comps/can.c
+++ stm32f303/src/comps/can.c
@@ -29,25 +29,6 @@ HAL_PIN(scale);
 HAL_PIN(udc);
 
 
-//#define TX_ADDRESS       0x0103     // address that is used for responding
-//#define RX_ADDRESS       0x0006       // address to listen to
-
-//#define MAX_SATURATED    0.2          // max. time in s position PID saturation is allowed
-//#define MAX_CURRENT      80           // max. motor current in 1/10 A
-
-//#define POSITION_OFFSET  0.0          // static position offset
-
-//#define SCALE            -2.0 * M_PI                // scaling factor for joint 1,3
-////#define SCALE            2.0 * M_PI                // scaling factor for joint 4,5
-////#define SCALE            -(2.0 * M_PI) / 24.3495    // scaling factor for joint roll plug (6)
-////#define SCALE            -0.0895353 //-89.5353                   // scaling factor for joint linear axis (2)
-
-////#define HOMING_VEL       0.05 	// For Linear axis (2)
-//#define HOMING_VEL       0.1		// For all else
-
-//#define PULLUP           GPIO_PULLUP  // GPIO_PULLDOWN for linear axis, GPIO_PULLUP for all else
-
-
 #define BOARD_ID  0x003
 
 // ID 1
@@ -85,7 +66,9 @@ HAL_PIN(udc);
 //#define POSITION_OFFSET  0.0204          // static position offset
 #define POSITION_OFFSET  0.0          // use Zero Offset, Ros will handle it
 #define SCALE            -2.0 * M_PI    // scaling factor for joint 1,3
-#define HOMING_VEL       0.1		// For all else
+// TODO PUT THIS BACK AFTER TESTING
+#define HOMING_VEL       0.3		// For all else
+//#define HOMING_VEL       0.1		// For all else
 #define PULLUP           GPIO_PULLUP  // GPIO_PULLDOWN for linear axis, GPIO_PULLUP for all else
 
 // ID 4
@@ -128,17 +111,22 @@ HAL_PIN(udc);
 #endif
 
 
-
-
+// Undervoltage threshold
 #define UVLO             15.0
 
+// Forward Decls
+void ledRed(uint8_t state);
+void ledBlue(uint8_t state);
+void ledGreen(uint8_t state);
+void testTransmit(unsigned char * foo);
 
 
 uint8_t errors = 0b00000000; // 0: motor disconnected / 1: motor short / 2: position error / 3: overcurrent / 4: undervoltage / 5: overvoltage / 6: CAN timeout / 7: hardfault
+uint8_t reported_errors = 0b00000000; // 0: motor disconnected / 1: motor short / 2: position error / 3: overcurrent / 4: undervoltage / 5: overvoltage / 6: CAN timeout / 7: hardfault
 uint8_t current = 0;         // motor current in 1/10 A (100mA / LSB)
 
 uint8_t running = 0;
-uint8_t mode = 0; //0 = pos, 1 = vel , 2 = homing positive , 3 = homin negative
+volatile uint8_t mode = 0; //0 = pos, 1 = vel , 2 = homing positive , 3 = homin negative
 uint8_t indexPin = 0;
 
 uint32_t timeout = 0;
@@ -147,7 +135,7 @@ float pos = 0, vel = 0, pos_in = 0, vel_in = 0, txPos = 0;
 
 uint8_t enable = 0;
 
-int8_t homing = 0;
+volatile int8_t homing = 0;
 
 float homingOffset = 0;
 
@@ -275,91 +263,72 @@ void setEnable(int32_t enable_)
   }
 }
 
-// Sets the desired P Value for the controller based on the currend mode
-// DEPRECATED, USE THE setMode NOW
-void setPValue()
+inline void setMode(uint32_t desired_mode)
 {
-  // mode has to be set before
-  // mode 0 = Position
-  // mode 1 = Velocity
 
-  if(mode == 0)
-  {
-    if (BOARD_ID == 0x002 || BOARD_ID == 0x006)
-    {
-            hal_parse("ypid0.pos_p = 7"); // Was 7 for 6 and 2 0.4 for all else
-    }
-    else
-    {
-        hal_parse("ypid0.pose_p = 0.4"); // TODO: Figure out what hal parese cann actually work with and make this better
-    }
-  }
-  else
-  {
-    hal_parse("ypid0.pose_p = 0");
-  }
-
-}
-
-void setMode(uint32_t desired_mode)
-{
   // Sets the mode to either
   // 0 = Position
   // 1 = Velocity
   // 2 = Positive Homing in Velcity mode
   // 3 = Inverse Homing in Velocity mode
-  mode = desired_mode;
+  //mode = desired_mode;
 
   switch (desired_mode)
   {
     // Position
     case 0:
-       mode = 0;
+      mode = 0;
+//     setPValue();  // Sets P accordingy
+//     hal_parse("ypid0.vel_ext_cmd = vel1.vel"); // Set the comand to ... whatever
+
       if (BOARD_ID == 0x002 || BOARD_ID == 0x006)
       {
         hal_parse("ypid0.pos_p = 7"); //7 for 6 and 2 0.4 for all else
       }
       else
       {
-         hal_parse("ypid0.pose_p = 0.4");
+        hal_parse("ypid0.pos_p = 0.4");
       }
       hal_parse("ypid0.vel_ext_cmd = vel1.vel"); // Set the comand to the the output of the velocity cascade i think?
 
       // Activate Blue LED -> Resulting in Violet when active and Blue when inactive
       ledBlue(1);
       ledGreen(0);
+      testTransmit(00);
       break;
     // Velocity
     case 1:
       mode = 1;
-      hal_parse("ypid0.pose_p = 0");
+      hal_parse("ypid0.pos_p = 0");
       hal_parse("ypid0.vel_ext_cmd = linrev0.cmd_d_out"); // Set the Velcotiy to the scaled input
 
       // Activate green LED -> Resulting in Yellow when active and Green when inactive
       ledBlue(0);
       ledGreen(1);
+      testTransmit(01);
       break;
     // Positive homing (velocity)
     // Currently unused
     case 2:
-      mode = 1;
-      hal_parse("ypid0.pose_p = 0");
+      //mode = 1;
+      hal_parse("ypid0.pos_p = 0");
       hal_parse("ypid0.vel_ext_cmd = linrev0.home_d_out");
-      // Activate green LED -> Resulting in Yellow when active and Green when inactive
-      ledBlue(0);
-      ledGreen(0);
-      ledRed(0);
+
+      ledBlue(1);
+      ledGreen(1);
+      testTransmit(02);
       break;
     // Negative homing (veocity)
     // currently unused
     case 3:
-      mode = 1;
-      hal_parse("ypid0.pose_p = 0");
+      //mode = 1;
+      hal_parse("ypid0.pos_p = 0"); //P Was already set
       hal_parse("ypid0.vel_ext_cmd = linrev0.home_neg_d_out");
       // Activate green LED -> Resulting in Yellow when active and Green when inactive
-      ledBlue(0);
-      ledGreen(0);
-      ledRed(0);
+       ledBlue(1);
+      ledGreen(1);
+
+      testTransmit(03);
       break;
     // Velocity
     default:
@@ -368,7 +337,8 @@ void setMode(uint32_t desired_mode)
       hal_parse("ypid0.vel_ext_cmd = linrev0.cmd_d_out"); // Set the Velcotiy to the scaled input
       // Activate green LED -> Resulting in Yellow when active and Green when inactive
       ledBlue(0);
-      ledGreen(1);
+      ledGreen(0);
+      testTransmit(04);
       break;
   }
 }
@@ -376,33 +346,50 @@ void setMode(uint32_t desired_mode)
 // Resets the control values to initial defaults
 void resetControlValues()
 {
+
+  // Reset homing to none Homing
   homing = 0;
+  // Set the Velocity to 0
   vel = 0;
-  // Set Velocity as default
-  setMode(0);
+
+  // Set the p Values for the mode which we are currently in
+  // If the current mode was one that is doing a reset -> Set to standrd velocity mode
+  if (mode > 2)
+  {
+    mode = 0;
+  }
+  setMode(mode);
+
   // Disable by default (in case an enable is sent inthe samemessage it willbe evaluated after the error clear and shouldbe applied immediately
   setEnable(0);
 }
 
 
 void CAN_rdMsg (uint32_t ctrl, CAN_msg *msg)  {
-                                              /* Read identifier information  */
-  if ((CAN->sFIFOMailBox[0].RIR & CAN_ID_EXT) == 0) {
+  /* Read identifier information  */
+  if ((CAN->sFIFOMailBox[0].RIR & CAN_ID_EXT) == 0)
+  {
     msg->format = STANDARD_FORMAT;
     msg->id     = 0x000007FF & (CAN->sFIFOMailBox[0].RIR >> 21);
-  } else {
+  }
+  else
+  {
     msg->format = EXTENDED_FORMAT;
     msg->id     = 0x1FFFFFFF & (CAN->sFIFOMailBox[0].RIR >> 3);
   }
-                                              /* Read type information        */
-  if ((CAN->sFIFOMailBox[0].RIR & CAN_RTR_REMOTE) == 0) {
+  /* Read type information        */
+  if ((CAN->sFIFOMailBox[0].RIR & CAN_RTR_REMOTE) == 0)
+  {
     msg->type =   DATA_FRAME;
-  } else {
+  }
+  else
+  {
     msg->type = REMOTE_FRAME;
   }
-                                              /* Read number of rec. bytes    */
+
+  /* Read number of rec. bytes    */
   msg->len     = (CAN->sFIFOMailBox[0].RDTR      ) & 0x0F;
-                                              /* Read data bytes              */
+  /* Read data bytes              */
   msg->data[0] = (CAN->sFIFOMailBox[0].RDLR      ) & 0xFF;
   msg->data[1] = (CAN->sFIFOMailBox[0].RDLR >>  8) & 0xFF;
   msg->data[2] = (CAN->sFIFOMailBox[0].RDLR >> 16) & 0xFF;
@@ -415,23 +402,30 @@ void CAN_rdMsg (uint32_t ctrl, CAN_msg *msg)  {
 
   CAN->RF0R |= CAN_RF0R_RFOM0;             /* Release FIFO 0 output mailbox */
 
-  if (msg->len == 8) {
+  if (msg->len == 8)
+  {
 
     // Bit 7 Clears the errors
-    if ((msg->data[0] >> 7)  & 0x01) {
+    if ((msg->data[0] >> 7)  & 0x01)
+    {
       //clear errors if they were present
-      if (errors != 0) {
-        resetControlValues();
+      if (errors != 0)
+      {
+        //resetControlValues();
         hal_parse("start");
         errors = 0;
+        reported_errors = 0;
+        testTransmit(9);
       }
     }
 
     // Enable Flag
-    if (((msg->data[0] >> 4)  & 0x01) && errors == 0) { //arm motor
+    if (((msg->data[0] >> 4)  & 0x01) && errors == 0)
+    {
       setEnable(1);
     }
-    else {
+    else
+    {
       setEnable(0);
     }
 
@@ -440,86 +434,28 @@ void CAN_rdMsg (uint32_t ctrl, CAN_msg *msg)  {
       uint8_t b[] = {msg->data[4], msg->data[3], msg->data[2], msg->data[1]};
       // The actual position is Copied to the pos variable for later use
       memcpy(&pos, &b, sizeof(pos));
-
-      // In Case of velocity mode, activate the position mode and set a P value for that... (TODO: Use the Default P Value instead)
-      if (mode == 1) {
-        setMode(0);
-//        mode = 0;     // Sets mode to Position
-//        setPValue();  // Sets P accordingy
-//          hal_parse("ypid0.vel_ext_cmd = vel1.vel"); // Set the comand to ... whatever
-
-//        // Togheter with RED (Armed) this will result in Violet
-//        ledBlue(1);
-//        ledGreen(0);
-      }
+      vel = 0;
+      setMode(0);
     }
     // Velocity is set
     else if ((msg->data[0] >> 1)  & 0x01) { //set velocity
       uint8_t b[] = {msg->data[4], msg->data[3], msg->data[2], msg->data[1]};
       memcpy(&vel, &b, sizeof(vel));
 
-      // In Case the previous mode was Position mode, change to velocity by deactivating the position controller
-      if (mode == 0) {
-        setMode(1);
-//        mode = 1;
-//        setPValue();
-//          hal_parse("ypid0.vel_ext_cmd = linrev0.cmd_d_out");
-
-//        // Together with RED (Armed) this will result in yellow
-//        ledBlue(0);
-//        ledGreen(1);
-      }
+      setMode(1);
     }
 
 
     if (((msg->data[0] >> 2)  & 0x01)) { //home joint
     // Home is requested
-
-      // If Homing is curently Active (antything but 0)
-      if (homing != 0) {
-        homing = 0;
-        if (mode == 0) {
-          setMode(0);
-//          setPValue();
-//          hal_parse("ypid0.vel_ext_cmd = vel1.vel");
-
-//          ledBlue(1);
-//          ledGreen(0);
-        }
-
-        else if (mode == 1) {
-          setMode(1);
-//          setPValue();
-//          hal_parse("ypid0.vel_ext_cmd = linrev0.cmd_d_out");
-
-//          ledBlue(0);
-//          ledGreen(1);
-        }
-      }
-
-      // Set the mode to velocity
-     // mode = 1;
-      // Change p values accordingly:
-     // setPValue();
-
-      // Activate the Green LED (leave the enabled flag (red) as is)
-     //ledGreen(1);
-      //ledBlue(0);
-      //ledRed(0);
-
-      // Activate velotiy mode
-      //setMode(1);
-
       // Direction is set to a direction 1 = positive idrection -1 = negatice direction
       if ((msg->data[0] >> 3)  & 0x01) { //set direction
-        homing = 1;
-        //setMode(2);
+          setMode(2);
       }
       else {
-        homing = -1;
-        //setMode(3);
+        setMode(3);
       }
-      //homing = 1;
+       homing = 3;
     }
 
 
@@ -574,37 +510,48 @@ void read_CAN(void)
   CAN_rdMsg (1, &CAN_RxMsg);
 }
 
-void testTransmit(char * foo) {
+void testTransmit(unsigned char * foo) {
   hcan.pTxMsg = &myTxMessage;
 
   myTxMessage.DLC = 4;
   myTxMessage.StdId = TX_ADDRESS;
   myTxMessage.IDE = CAN_ID_STD;
-  myTxMessage.Data[0] = 0xDE;
-  myTxMessage.Data[1] = 0xAD;
-  myTxMessage.Data[2] = 0xBE;
-  myTxMessage.Data[3] = 0xEF;
+  myTxMessage.Data[0] = 0xFF;
+  myTxMessage.Data[1] = 0xFF;
+  myTxMessage.Data[2] = 0xFF;
+  myTxMessage.Data[3] = foo;
 
   HAL_CAN_Transmit(&hcan, HAL_MAX_DELAY);
 }
 COMMAND("cantx", testTransmit);
 
 void sendError() {
-  hcan.pTxMsg = &myTxMessage;
+//  if (errors != reported_errors)  // Only actively send out the errors if the reported errors are differnt (i.e. we did not send the error before)
+//  {
 
-  myTxMessage.DLC = 8;
-  myTxMessage.StdId = TX_ADDRESS;
-  myTxMessage.IDE = CAN_ID_STD;
-  myTxMessage.Data[0] = 0;
-  myTxMessage.Data[1] = 0;
-  myTxMessage.Data[2] = 0;
-  myTxMessage.Data[3] = 0;
-  myTxMessage.Data[4] = 0;
-  myTxMessage.Data[5] = current;
-  myTxMessage.Data[6] = errors;
-  myTxMessage.Data[7] = 0;
+    hcan.pTxMsg = &myTxMessage;
 
-  HAL_CAN_Transmit(&hcan, HAL_MAX_DELAY);
+    myTxMessage.DLC = 8;
+    myTxMessage.StdId = TX_ADDRESS;
+    myTxMessage.IDE = CAN_ID_STD;
+    myTxMessage.Data[0] = 0;
+    myTxMessage.Data[1] = 0;
+    myTxMessage.Data[2] = 0;
+    myTxMessage.Data[3] = 0;
+    myTxMessage.Data[4] = 0;
+    myTxMessage.Data[5] = current;
+    myTxMessage.Data[6] = errors;
+    myTxMessage.Data[7] = 0;
+
+    HAL_CAN_Transmit(&hcan, HAL_MAX_DELAY);
+
+    reported_errors = errors;
+//  }
+
+    // RED!
+    ledBlue(0);
+    ledGreen(0);
+    ledRed(1);
 }
 
 // Finished homing, report back to sender
@@ -679,7 +626,7 @@ static void nrt_func(float period, volatile void * ctx_ptr, volatile hal_pin_ins
  //Check for Saturation of controller
   if (PIN(saturated) > MAX_SATURATED && running) {
     //TX
-    hal_parse("stop");
+//    hal_parse("stop");
     errors = errors | 1 << 2;
     sendError();
     printf("saturated!\n");
@@ -688,13 +635,13 @@ static void nrt_func(float period, volatile void * ctx_ptr, volatile hal_pin_ins
    // Check for overcurrent error
   if (current > MAX_CURRENT && running) {
     //TX
-    hal_parse("stop");
+//    hal_parse("stop");
     errors = errors | 1 << 3;
     sendError();
     printf("current!\n");
   }
 
-  // Detecting an undervoltage is a special case as the controller needs to keep running.. (why?)
+   //Detecting an undervoltage is a special case as the controller needs to keep running.. (why?)
 
   if (PIN(udc) < UVLO && running)
   {
@@ -722,25 +669,6 @@ static void nrt_func(float period, volatile void * ctx_ptr, volatile hal_pin_ins
   // Actually write things based on the enable status
   PIN(enable) = enable;
 
-  // in case homing was started (state 1) we convert the velocity based on the scale
-
-  if (homing == 1) {
-    // TODO: FOR SOME REASON THIS IS NOT WORKING WITH MODESET... WHY?
-    homing = 3;
-    hal_parse("ypid0.pos_p = 0"); //P Was already set
-    hal_parse("ypid0.vel_ext_cmd = linrev0.home_d_out");
-//    setMode(2);
-  }
-  // Also velcity conversion
-  else if (homing == -1) {
-    // TODO: FOR SOME REASON THIS IS NOT WORKING WITH MODESET... WHY?
-    homing = 3;
-    hal_parse("ypid0.pos_p = 0"); //P Was already set
-    hal_parse("ypid0.vel_ext_cmd = linrev0.home_neg_d_out");
-//    setMode(3);
-  }
-
-
   // homing state 2 -> Homing is finished -> Drive to theposition in question
   if (homing == 2) {
 
@@ -758,28 +686,8 @@ static void nrt_func(float period, volatile void * ctx_ptr, volatile hal_pin_ins
     // Set the Output Position of this Module to the ucrrent homing offset (aka the current value that the encoders have)
     PIN(pos) = homingOffset;
 
-    // Homing finished, in case that we wre are in Position mode:
-//    if (mode == 0) {
-////      setMode(0); // Should not happen
-//        setPValue();
-//        hal_parse("ypid0.vel_ext_cmd = vel1.vel");
-
-//       ledBlue(1);
-//       ledGreen(0);
-//    }
-//    // Of if in velocity mode:
-//    else if (mode == 1) {
-////      setMode(1); // We EXPLICITLY set the mode aggain to convert all scalings back to their regular mode (the requested mode was actually a different one)
-//        setPValue();
-//       hal_parse("ypid0.vel_ext_cmd = linrev0.cmd_d_out");
-
-//       ledBlue(0);
-//       ledGreen(1);
-//    }
-
-    // Set Position mode Deliberately
-    uint32_t temp_mode = mode;
-    setMode(0);
+    // Restore the previously set mode (to get rid of the homing params)
+    setMode(mode);
 
     // if POS (note that pos is the current value given as position input. So during homing, this actually means it is a homingOffset to move to after the homing)
     if (pos != 0 && mode == 0) {
@@ -805,9 +713,6 @@ static void nrt_func(float period, volatile void * ctx_ptr, volatile hal_pin_ins
       }
     }
 
-    // reset mode to the one it was bevore
-    setMode(mode);
-
     homing = 0;
     doneHoming();
     printf("done homing!\n");
@@ -832,8 +737,7 @@ static void rt_func(float period, volatile void * ctx_ptr, volatile hal_pin_inst
   indexPin = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10);
   PIN(home) = indexPin;
 
-  // Set the Velocity to the currenty desired velocity, this should always be done as we would otherwise be incapable of restting the velocityy during homing
-  PIN(vel) = vel;
+
 
   // Check if any kind of homing is active
   if (homing != 0) {
@@ -880,6 +784,7 @@ static void rt_start(float period, volatile void * ctx_ptr, volatile hal_pin_ins
   struct can_pin_ctx_t * pins = (struct can_pin_ctx_t *)pin_ptr;
   running = 1;
   errors = 0;
+  reported_errors = 0;
 }
 
 static void rt_stop(float period, volatile void * ctx_ptr, volatile hal_pin_inst_t * pin_ptr){
